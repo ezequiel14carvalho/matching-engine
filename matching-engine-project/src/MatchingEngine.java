4a2c0a0272736e6c4c32514706fc96a1ef1f867e
Ezequiel Gomes de Carvalho <ezequielcarvalhop14@gmail.com>
2026-08-25 14:28:38 -0300

    Update MatchingEngine.java
    Estava com muita dificuldade em fazer a matcharket, então achei melhor em dividir ao máximo as tarefas. Criei uma processOrder apenas para saber o tipo da ordem, uma executeMatch para fazer o trade e por fim a minha matchMarket, que apenas recebe a ordem ja no tipo market, pega o melhor preço, que será a primeira chave independentimente se a order for do tipo buy ou sell e chama a minha executeMatch.

--- matching-engine-project/src/MatchingEngine.java
+++ matching-engine-project/src/MatchingEngine.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+
 public class MatchingEngine {
     private OrderBook orderBook = new OrderBook();
 
@@ -5,13 +7,62 @@ public class MatchingEngine {
         this.orderBook = orderBook;
     }
 
-    // metodo para execução de ordem tipo market
-    private void executeMarketOrder(Order marketOrder) {
-        if (marketOrder.getSide() == Order.Side.BUY) {
-            // Ele tentará comprar até que ele venda tudo ou o orderBook de vendas esteja vazio
-            while (marketOrder.getQuantity() > 0 && !orderBook.getAsks().isEmpty()) {
-                
-            }
+    public void processOrder(Order order) {
+        if (order.getType() == Order.Type.MARKET) {
+            matchMarket(order);
+        } else {
+            matchLimit(order);
         }
     }
+
+    private void executeMatch(Order incoming, Queue<Order> queue, double price) {
+        int tradedTotal = 0;
+        while (incoming.getQuantity() > 0 && !queue.isEmpty()) {
+            Order resting = queue.peek();
+
+            // necessário pois há casos onde a quantidade de compras é diferente
+            // da quantidade de vendas disponiveis, e vice-versa.
+            int qty = Math.min(incoming.getQuantity(), resting.getQuantity());
+
+            tradedTotal += qty;
+            incoming.setQuantity(incoming.getQuantity() - qty);
+            resting.setQuantity(resting.getQuantity() - qty);
+
+            // se não restou nada na nossa ordem, ent~ao removemos ele do noss orderBook
+            if (resting.getQuantity() == 0) {
+                queue.poll();
+                orderBook.removeOrder(resting.getId());
+            }
+        }
+        System.out.println("Trade, price: " + price + ", qty: " + tradedTotal);
+    }
+
+    // metodo para execução de ordem tipo market
+    private void matchMarket(Order order) {
+        if (order.getSide() == Order.Side.BUY) {
+            while (order.getQuantity() > 0 && !orderBook.getAsks().isEmpty()) {
+                // a primeira chave de uma treemap é a o menor valor
+                double bestPrice = orderBook.getAsks().firstKey();
+
+                // agora que temos o melhor preçp/chave
+                // vamos pegar a fila correspondente
+                Queue<Order> queue = orderBook.getAsks().get(bestPrice);
+
+                // agora executamos o trade
+                executeMatch(order, queue, bestPrice);
+                // caso a fila do preço/chave ficou vazia, limpamos
+                if (queue.isEmpty()) orderBook.getAsks().remove(bestPrice);
+            }
+        } else {
+            // agora a ordem é do tipo sell e atacaremos o bid
+            // como invertemos na propia classe, podemos pegar a primeira key
+            double bestPrice = orderBook.getBids().firstKey();
+
+            Queue<Order> queue = orderBook.getBids().get(bestPrice);
+
+            executeMatch(order, queue, bestPrice);
+            if (queue.isEmpty()) orderBook.getBids().remove(bestPrice);
+        }
+    }
+
 }
